f6dacce8088fd37ca29a237bd249475875d5923b
Libero Lance <mfc2294@gmail.com>
2022-04-06 13:47:51 -0400

    Add outline for rest of project

--- javascript.js
+++ javascript.js
@@ -16,5 +16,19 @@ function computerPlay() {
   }
 }
 
+/*playRound(playerChoice, computerChoice)
+  takes the player's choice and compares to computers, returns string declaring a winner.
+  1. Map out conditions
+  2. return string declaring winner
+*/
+
+/*game()
+  plays a game of 5 rounds, tracks the score and reports the winner at the end
+  1. loop 5 times calling playRound()
+  2. prompt() the user to update their choice for each round
+  3. increment a counter for each number of wins
+  4. compare winnings and declare a winner after the loop ends
+*/
+
 /*test code here*/
 console.log(computerPlay())
