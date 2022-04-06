da62fd00a5eb6d592b142be95c30e00713402ad5
Libero Lance <mfc2294@gmail.com>
2022-04-06 13:28:14 -0400

    Write and test computerPlay()

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+/*Rock Paper Scissors*/
+
+/*computerPlay()
+  Randomly picks 'Rock' 'Paper' 'Scissors' and returns that string
+  1. Generate random int from 1 to 3
+  2. if 1 return rock, 2 return paper, 3 return scissors 
+*/
+function computerPlay() {
+  let randNum = Math.floor(Math.random() * 3) + 1;
+  if(randNum === 1) {
+    return 'rock';
+  } else if(randNum === 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+/*test code here*/
+console.log(computerPlay())
